a69e1888fa19e9eb825d7b7e52cec4d29878bcec
Dgordg85 <dgordg85@gmail.com>
2016-08-29 09:05:04 +0300

    List

--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -12,11 +12,32 @@ namespace Snake
         {
             Point p1 = new Point(1, 3, '*');
             p1.Draw();
-          
+
             Point p2 = new Point(4, 5, '#');
             p2.Draw();
 
             Console.ReadLine();
+
+            List<int> numlist = new List<int>();
+            numlist.Add(0);
+            numlist.Add(1);
+            numlist.Add(2);
+
+            int x = numlist[0];
+            int y = numlist[1];
+            int z = numlist[2];
+
+            foreach(int i in numlist)
+            {
+                Console.WriteLine(i);
+            }
+
+            numlist.Remove(0);
+
+            List<Point> plist = new List<Point>;
+            plist.Add(p1);
+            plist.Add(p2);
+
         }
     }
 }
